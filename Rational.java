9b0cf621422650c462b6c74046bacb4b3cf0110b
nperrotti <nicholas.perrotti79@gmail.com>
2013-11-18 13:23:33 -0500

    Update Rational.java

--- Rational.java
+++ Rational.java
@@ -4,7 +4,22 @@ public class Rational{
     private int denominator;
     
     public Rational(){
-    
+        numerator = 0;
+        denominator = 1;
     }
+    
+    public Rational( int n, int d ){
+        numerator = n;
+        if ( denominator == 0){
+            System.out.println("Error: Cannot have zero denominator");
+            numerator = 0;
+            denominator = 1;
+        }
+        else {
+            denominator = d;
+        }
+    }
+    
+    
 
 }
